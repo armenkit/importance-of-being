f3be94d2fbd97923863fd115d92234bf7142d64c
Armen Suny <armen@kitcrm.com>
2015-02-14 19:19:05 -0800

    bastardizes great literature

--- importance.rb
+++ importance.rb
@@ -5,6 +5,7 @@ configure do
   set :haml, format: :html5
 end
 
-get '/' do
-  haml :index
+get '/?:ernest?' do
+  ernest = params[:ernest] || 'Ernest'
+  haml :index, locals: { ernest: ernest }
 end
